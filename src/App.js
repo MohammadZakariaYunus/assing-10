8564ad398235c0578894f945a899a3352f14d31d
MohammadZakariaYunus <mohammadzakariaabdullahyunus99@gmail.com>
2022-04-18 19:46:37 +0600

    Components Done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Header from '../src/Pages/Shared/Header/Header';
 import './App.css';
+import Footer from './Pages/Shared/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
+      <Footer></Footer>
 
     </div>
   );
