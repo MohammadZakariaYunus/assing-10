5403a142a6fdbe2ba7ed2541ebf42a83314ba9af
MohammadZakariaYunus <mohammadzakariaabdullahyunus99@gmail.com>
2022-04-19 13:48:53 +0600

    Register done

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import { Route, Routes } from 'react-router-dom';
 import Header from '../src/Pages/Shared/Header/Header';
 import './App.css';
 import Home from './Pages/Home/Home';
-import NotFound from './Pages/NotFound/NotFound';
 import Service from './Pages/Service/Service';
+import About from './Pages/About/About';
+import Login from './Pages/Login/Login/Login';
+import Register from './Pages/Login/Register/Register';
 import Footer from './Pages/Shared/Footer/Footer';
+import NotFound from './Pages/NotFound/NotFound';
 
 function App() {
   return (
@@ -15,6 +18,10 @@ function App() {
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/home" element={<Home></Home>}></Route>
         <Route path='/service' element={<Service></Service>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/service' element={<Register></Register>}></Route>
+
         <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
